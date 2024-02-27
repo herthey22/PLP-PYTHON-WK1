76f91214930d7f1a951a23fb7e07c5ff7c9c257d
herthey22 <hertheytutu@gmail.com>
2024-02-26 22:12:51 -0800

    first commit

---
+++ user_input.py
@@ -0,0 +1,27 @@
+# user_input.py
+
+#  for user's name input
+name = input("Enter your name: ")
+
+#  for user's age
+age = input("Enter your age: ")
+# Ask for user's location
+location = input("Enter your location: ")
+
+# Print out personalized message
+print("Hello " + name + ", you are " + age + " years old and you live in " + location + ".")
+
+
+# when you run it you will be told to enter the following:
+"""
+Enter your name: Ade
+Enter your age: 24
+Enter your location: Oyo, Nigeria
+
+"""
+
+#output
+"""
+# print("Hello " + name + ", you are " + age + " years old and you live in " + location + ".")
+Hello Ade, you are 24 years old and you live in Oyo.
+"""
